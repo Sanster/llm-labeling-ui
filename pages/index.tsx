7f9f336a2aab2e0ae44cab80e1467cb656a5f98f
Qing <cwq1913@gmail.com>
2023-09-12 10:41:00 +0800

    hide prompt bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -503,7 +503,7 @@ const Home = () => {
               <Chat stopConversationRef={stopConversationRef} />
             </div>
 
-            <Promptbar />
+            {/* <Promptbar /> */}
           </div>
         </main>
       )}
